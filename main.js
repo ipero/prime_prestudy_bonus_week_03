0e21282f3aaf446d1a457f4b78a001f15e6d631a
Roman H <ipero@live.com>
2016-01-31 22:23:01 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function squareNum (num){
+	return num*num;
+}
+function doubleNum(num){
+	return 2*num;
+}
+function tripleNum(num){
+	return 3*num;
+}
+console.log(doubleNum(tripleNum(squareNum(2))));
